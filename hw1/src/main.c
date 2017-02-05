789fbce9b77bd95e601fecfb8e2c48a26c2f8d9f
Arjun Rao <arjun.rao@stonybrook.edu>
2017-02-04 23:56:49 -0500

    nothing rly changed, just a return statement

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -6,7 +6,11 @@ int main(int argc, char **argv) {
     FILE* out;
 
     /* Note: create a variable to assign the result of validargs */
-    validargs(argc, argv, &in, &out);
-
-    return EXIT_SUCCESS;
+    int mode = validargs(argc, argv, &in, &out); //int or char?
+    if (mode == 0) {
+    	//USAGE(mode);
+    	return EXIT_FAILURE;
+    }
+    else
+    	return EXIT_SUCCESS;
 }
